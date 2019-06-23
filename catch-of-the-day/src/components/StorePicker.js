226431e6686641673b72b9667a014617cbc42cb3
Derek <derek@dadian-smith.net>
2019-06-23 17:09:31 -0500

    Hook up input to router

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { getFunName } from '../helpers.js'
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = e => {
+    e.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     )
