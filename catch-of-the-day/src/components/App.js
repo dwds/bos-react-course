e12355e84b45eaf70cd2e33a2521180ded0f6037
Derek <derek@dadian-smith.net>
2019-06-24 14:28:28 -0500

    Implement state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,27 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const fishes = {...this.state.fishes};
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({
+      fishes: fishes
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Order></Order>
-        <Inventory></Inventory>
+        <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
